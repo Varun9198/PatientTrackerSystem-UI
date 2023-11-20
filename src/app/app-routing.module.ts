2adfe91d2c27df059493e58ee4358111de8c82a8
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-11-20 16:51:25 -0500

    Feat: Created a basic design of Signup page for Doctors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {LandingPageComponent} from './landing-page/landing-page.component'
 import {LoginDoctorComponent} from './login-doctor/login-doctor.component'
 import { LoginPatientComponent } from "./login-patient/login-patient.component";
+import { SignupDoctorComponent } from './signup-doctor/signup-doctor.component';
 
 const routes: Routes = [
-  {path: '', component: LandingPageComponent},
+  // {path: '', component: LandingPageComponent},
+  {path: '', component: SignupDoctorComponent},
   {path: 'DoctorLogin', component: LoginDoctorComponent},
   {path: 'PatientLogin', component: LoginPatientComponent}
 ]
---
+++ src/app/signup-doctor/signup-doctor.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupDoctorComponent } from './signup-doctor.component';
+
+
+
+@NgModule({
+  declarations: [
+    SignupDoctorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SignupDoctorModule { }
