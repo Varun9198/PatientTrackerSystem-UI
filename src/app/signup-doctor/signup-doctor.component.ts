05027650bd281f768d99dd7f02cfde8d794a3615
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-11-22 09:27:18 -0500

    Refactored the signup forms for ensuring valid inputs

--- src/app/signup-doctor/signup-doctor.component.ts
+++ src/app/signup-doctor/signup-doctor.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from "@angular/router";
 
 @Component({
@@ -7,8 +8,32 @@ import { Router } from "@angular/router";
   styleUrls: ['./signup-doctor.component.css']
 })
 export class SignupDoctorComponent {
-  constructor(private router: Router){}
+  
+  form:FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    name: ['', Validators.required],
+    dob: ['', Validators.required],
+    hospital: ['', Validators.required],
+    speciality: ['', Validators.required],
+    address: ['', Validators.required],
+    phone_number: ['', Validators.required],
+    email: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder, private router: Router){}
+  
   signup(){
+    let username = this.form.value.username;
+    let password = this.form.value.password;
+    let name = this.form.value.name;
+    let dob = this.form.value.dob;
+    let hospital = this.form.value.hospital;
+    let speciality = this.form.value.speciality;
+    let address = this.form.value.address;
+    let phone_number = this.form.value.phone_number;
+    let email = this.form.value.email;
+
     alert('Account Created');
     this.router.navigateByUrl('DoctorLogin')
   }
--- src/app/signup-patient/signup-patient.component.ts
+++ src/app/signup-patient/signup-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from "@angular/router";
 
 @Component({
@@ -7,8 +8,31 @@ import { Router } from "@angular/router";
   styleUrls: ['./signup-patient.component.css']
 })
 export class SignupPatientComponent {
-  constructor(private router: Router){}
+
+  form:FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    name: ['', Validators.required],
+    dob: ['', Validators.required],
+    weight: ['', Validators.required],
+    height: ['', Validators.required],
+    address: ['', Validators.required],
+    phone_number: ['', Validators.required],
+    email: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder, private router: Router){}
+  
   signup(){
+    let username = this.form.value.username;
+    let password = this.form.value.password;
+    let name = this.form.value.name;
+    let dob = this.form.value.dob;
+    let weight = this.form.value.weight;
+    let height = this.form.value.height;
+    let address = this.form.value.address;
+    let phone_number = this.form.value.phone_number;
+    let email = this.form.value.email;
     alert('Account Created');
     this.router.navigateByUrl('PatientLogin')
   }
