dadc509ea95235d91e18f95326fa23bf845e748e
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-12-09 09:49:44 -0500

    Feat: Edit Case functionality for Doctor (Temp)

--- src/app/doctor-update-case/doctor-update-case.component.ts
+++ src/app/doctor-update-case/doctor-update-case.component.ts
@@ -19,12 +19,18 @@ interface CASES{
 })
 export class DoctorUpdateCaseComponent {
   form:FormGroup = this.fb.group({
-    appointment_number: ['', Validators.required]
+    appointment_number: ['', Validators.required],
+    name: ['', Validators.required],
+    username: ['', Validators.required],
+    email: ['', Validators.required]
   });
   Cases: CASES[] = UsersJson;
   constructor(private fb:FormBuilder){}
   reschedule(){
     let appointment_number = this.form.value.appointment_number;
+    let name = this.form.value.name;
+    let username = this.form.value.username;
+    let email = this.form.value.email;
     if (appointment_number<=0){
       alert('Invalid appointment id');
     }
